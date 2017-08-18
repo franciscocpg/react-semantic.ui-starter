9a26d60aa76cc346f09aa21389a0a8676565f987
Francisco Guimarães <francisco.cpg@gmail.com>
2017-08-18 18:41:29 -0300

    Fix up deploy script

--- _scripts/up-deploy.sh
+++ _scripts/up-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-# npm i && npm run build
+npm i && npm run build
 rm -rf node_modules up-deploy && mkdir up-deploy
 npm i --production
 cp -r dist node_modules _scripts/package.json up-deploy
